9b146343090916de40a6991aee7e9bc058004349
Islam Mohamed <68484416+IslamMohamed95@users.noreply.github.com>
2023-03-19 02:43:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ require("dotenv").config();
 PORT = process.env.SERVER_PORT;
 TZ = "Asia/Dubai";
 
-app.listen("https://confused-flip-flops-pike.cyclic.app/, () => {
+app.listen("https://confused-flip-flops-pike.cyclic.app/", () => {
   console.log(`Server is running on port: ${PORT}`);
 });
