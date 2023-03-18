f52c3e69211cc3036793feaf749c97208e5ea71d
Islam Mohamed <68484416+IslamMohamed95@users.noreply.github.com>
2023-03-19 02:52:10 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ require("dotenv").config();
 PORT = process.env.SERVER_PORT;
 TZ = "Asia/Dubai";
 
-app.listen(3000, () => {
+app.listen(3002, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
