7b9187c03cabb2d5e85ddce87368ad565782a0a8
islam <islammooma95@gmail.com>
2023-03-26 01:38:34 +0400

    ..

--- app/controllers/variations.controller.js
+++ app/controllers/variations.controller.js
@@ -62,7 +62,7 @@ class variation {
   static fileDownload = async (req, res) => {
     try {
       const file = await variationModel.findById(req.params.id);
-      console.log(file.file.filename);
+      console.log(file.file);
       res.download(file.file.filename);
     } catch (e) {
       res.status(500).send({
