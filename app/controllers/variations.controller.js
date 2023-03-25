6bede0eec516213537f6d7b83f56879116388ade
islam <islammooma95@gmail.com>
2023-03-26 01:39:37 +0400

    ..

--- app/controllers/variations.controller.js
+++ app/controllers/variations.controller.js
@@ -63,7 +63,7 @@ class variation {
     try {
       const file = await variationModel.findById(req.params.id);
       console.log(file.file);
-      res.download(file.file.filename);
+      res.download(`/temp/uploads/${file.file.originalname}`);
     } catch (e) {
       res.status(500).send({
         API: false,
