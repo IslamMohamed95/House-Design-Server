569334384f4c765d3dcf0da98c51d124c8fc5f5d
islam <islammooma95@gmail.com>
2023-03-26 04:46:06 +0400

    ..

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(express.static("./uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
