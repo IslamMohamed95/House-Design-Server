1bfb75bacf7bbaf43743dee843ad686917965957
islam <islammooma95@gmail.com>
2023-03-26 23:10:35 +0400

    ..

--- app/db/dbConnection.js
+++ app/db/dbConnection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 mongoose.connect(
   "mongodb+srv://islammooma95:271195@house-deisgn.k1syohe.mongodb.net/?retryWrites=true&w=majority",
   () => {
