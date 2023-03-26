30f2e5c1d57fe966ad9badd00d7d5bf610e39984
islam <islammooma95@gmail.com>
2023-03-26 23:14:07 +0400

    ..

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -4,9 +4,7 @@ const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    let loc;
-    if (!req.master) loc = "uploads/";
-    else loc = path.join("uploads", req.master._id.toString());
+    let loc = "uploads/";
     fs.mkdir(loc, (err) => {});
     cb(null, loc);
   },
