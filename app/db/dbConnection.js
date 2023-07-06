f048d6bd3ed8f7efa61f3eaab43c0a80a1aede94
islam <islammooma95@gmail.com>
2023-07-07 02:04:17 +0400

    ..

--- app/db/dbConnection.js
+++ app/db/dbConnection.js
@@ -3,12 +3,7 @@ const mongoose = require("mongoose");
 const Grid = require("gridfs-stream");
 
 mongoose.set("strictQuery", true);
-// mongoose.connect(
-//   "mongodb+srv://islammooma95:271195@house-deisgn.k1syohe.mongodb.net/?retryWrites=true&w=majority",
-//   () => {
-//     console.log("database is connected");
-//   }
-// );
+
 const conn = mongoose.createConnection(
   "mongodb+srv://islammooma95:271195@house-deisgn.k1syohe.mongodb.net/?retryWrites=true&w=majority"
 );
@@ -17,6 +12,7 @@ let gfs;
 conn.once("open", () => {
   gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection("variations");
+  console.log("Database is connected");
 });
 //"mongodb+srv://islammooma95:271195@house-deisgn.k1syohe.mongodb.net/?retryWrites=true&w=majority"
 //`${process.env.DB_PORT}${process.env.DB_NAME}`
--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -3,23 +3,6 @@ const crypto = require("crypto");
 const path = require("path");
 const { GridFsStorage } = require("multer-gridfs-storage");
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     let loc;
-//     loc = "upload";
-//     cb(null, loc);
-//   },
-//   filename: function (req, file, cb) {
-//     let name =
-//       file.fieldname + "-" + Date.now() + path.extname(file.originalname);
-//     cb(null, name);
-//   },
-// });
-
-// var upload = multer({
-//   storage,
-// });
-
 const storage = new GridFsStorage({
   url: "mongodb+srv://islammooma95:271195@house-deisgn.k1syohe.mongodb.net/?retryWrites=true&w=majority",
   file: (req, file) => {
