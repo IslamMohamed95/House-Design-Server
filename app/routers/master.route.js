173478645b4c2aa15f34a053a4cdc12aa69046f7
islam <islammooma95@gmail.com>
2023-07-07 00:29:34 +0400

    ..

--- app/routers/master.route.js
+++ app/routers/master.route.js
@@ -2,7 +2,7 @@ const route = require("express").Router();
 const auth = require("../middleware/authorization");
 const masterController = require("../controllers/master.controller");
 
-route.post("/register", auth("Master"), masterController.register);
+route.post("/register", masterController.register);
 route.post("/login", masterController.login);
 route.post("/reset", masterController.reset);
 route.post("/logout", auth("Master"), masterController.logout);
