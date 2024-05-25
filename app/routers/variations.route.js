2eb0d11595eb9612b87c7c7743b6307edec6c6ce
islam <islammooma95@gmail.com>
2024-05-25 15:02:50 +0400

    ..

--- app/routers/variations.route.js
+++ app/routers/variations.route.js
@@ -1,8 +1,7 @@
 const route = require("express").Router();
 const variationController = require("../controllers/variations.controller");
-const upload = require("../middleware/upload");
 
-route.post("/new/:id", upload.single("file"), variationController.new);
+route.post("/new/:id", variationController.new);
 route.get("/download/:name", variationController.Download);
 route.get("/variations", variationController.Variations);
 route.get("/contractVariations/:id", variationController.contractVariations);
