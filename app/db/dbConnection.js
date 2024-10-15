eaf98810d377e5ec23431dcdf92a26a356414157
islam <islammooma95@gmail.com>
2024-10-15 19:15:49 +0400

    ...

--- app/db/dbConnection.js
+++ app/db/dbConnection.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 //mongoose.set("strictQuery", true);
 
 mongoose.connect(
-  `mongodb+srv://islam:271195@housed.ims3c.mongodb.net/?retryWrites=true&w=majority&appName=Housed`,
+  `mongodb+srv://islam:271195@housed.ims3c.mongodb.net/?retryWrites=true&w=majority&appName=Housed&ssl=true`,
   () => {
     console.log("database is connected");
   }
