0b51eb6a7972376ea0b0a50d77646d4edc19f4b9
Islam Mohamed <68484416+IslamMohamed95@users.noreply.github.com>
2024-10-15 20:37:27 +0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app");
-require("dotenv").config();
-
-PORT = process.env.SERVER_PORT;
-
-app.listen(3000, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
