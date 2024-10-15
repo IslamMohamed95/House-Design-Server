bbe211d0d2a56c104244392a1523ce4f9d369780
islam <islammooma95@gmail.com>
2024-10-15 22:06:31 +0400

    ...

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const app = require("./src/app");
 require("dotenv").config();
 
 PORT = process.env.SERVER_PORT;
-TZ = "Asia/Dubai";
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server is running on port: ${3000}`);
 });
