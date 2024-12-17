0557063127d36ef6aca113e3f99e43a96ceedb9c
Islam Mohamed <68484416+IslamMohamed95@users.noreply.github.com>
2024-12-17 07:51:10 +0400

    Delete app directory

--- app/db/dbConnection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-const DB = process.env.DataBase;
-mongoose.connect(`${DB}`, () => {
-  console.log("Database is connected");
-});
--- app/routers/sales.route.js
+++
@@ -1,9 +0,0 @@
-const route = require("express").Router();
-const auth = require("../middleware/authorization");
-const salesController = require("../controllers/sales.controller");
-
-route.post("/new", auth("Master"), salesController.new);
-
-route.get("/all", auth("Master"), salesController.sales);
-
-module.exports = route;
